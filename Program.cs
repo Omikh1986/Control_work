13621f74a463ec77c2c260e8b90022ad27c3779f
Olga Mikhailova <Olyaya-l@yandex.ru>
2022-07-27 22:18:49 +0300

    added decision to Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] StartArray = { "hello", "2", "world", ":-)", "1234", "-2"};
+string[] FinalArray = new string[StartArray.Length];
+for (int i = 0, j = 0; i < StartArray.Length; i++, j++)
+{
+    if (StartArray[i].Length <= 3)
+    {
+        FinalArray[j] = StartArray[i];
+    }
+    Console.Write($"{FinalArray[j]} ");
+}
+
